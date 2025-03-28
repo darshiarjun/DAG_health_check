6f8bf75cf7c14a67c056db01bf83ddfc70f99247
Arjun P Darshi <arjunpdarshi@Mac.attlocal.net>
2025-03-28 04:17:04 -0500

    Modified host address

--- web_api_dag.py
+++ web_api_dag.py
@@ -123,4 +123,4 @@ if __name__ == "__main__":
     # Creating the Directed graph of the nodes"
     graph = nx.DiGraph()
     # Calling Flask instance on local host and port 8080
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
